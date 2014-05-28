654ba047fd54c92df4179d697936be806d5ef67b
Jess2D <jessicap_silveira@hotmail.com>
2014-05-28 14:21:36 -0300

    Primeira etapa do compilador : Analise Lexica

---
+++ LFT/src/teste/Main.java
@@ -0,0 +1,16 @@
+
+package teste;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author gilcley silva and jessica silveira
+ */
+public class Main {
+    public static void main(String[] args) throws IOException  {
+     Teste teste= new Teste();
+     teste.iniciar();
+    }
+    
+}
---
+++ LFT/src/teste/Teste.java
@@ -0,0 +1,41 @@
+package teste;
+
+import java.io.*;
+import portugol.lexer.*;
+import portugol.node.*;
+
+/**
+ *
+ * @author gilcley silva and Jessica silveira
+ */
+public class Teste {
+
+    public void iniciar() throws FileNotFoundException, IOException {
+        //carrega o arquivo
+        File f = new File("C:\\Users\\gilcley silva\\Documents\\NetBeansProjects\\LFT\\src\\teste\\t.txt");
+        Reader arq_teste;
+        try {
+            arq_teste = new FileReader(f);
+            MyLexer lexer = new MyLexer(new PushbackReader(arq_teste));
+            do {
+                Token token = lexer.next();
+                String nome = token.getClass().getSimpleName();
+                if (!nome.equals("EOF")) {
+                    nome = nome.substring(1, nome.length());
+                }
+                if (token instanceof TBlanks) {
+                    //System.out.print(token.getText());
+                } else {
+                    System.out.print(nome + "\n");
+                }
+                //caso seja o final do arquivo ele para
+                if (token instanceof EOF) {
+                    break;
+                }
+            } while (true);
+        } catch (LexerException ex) {
+            System.out.println(" ERRO lexico !!!");
+        }
+
+    }
+}
